998563cac7d68cde8ee4d05974f6102670fbe9f6
KajiiNarumi <kajiikana69@gmail.com>
2017-01-12 22:16:49 -0600

    apoyo para la gestion de cibercafes y escuelas
    script creado para el uso sobre distros basadas en arch y debian

---
+++ unlock.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export DISPLAY=:0
+
+xinput set-int-prop 12 "Device Enabled" 8 1
+xinput set-int-prop 11 "Device Enabled" 8 1
+xinput set-int-prop 10 "Device Enabled" 8 1
+xinput set-int-prop 9 "Device Enabled" 8 1
+xinput set-int-prop 8 "Device Enabled" 8 1
+xinput set-int-prop 7 "Device Enabled" 8 1
+
+
+
+
+#desmarque la opcion que alla seleccionado 
+#videos
+#pkill eog
+#fotos
+#pkill mplayer
+
